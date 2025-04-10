49ea670030ab772fbde424cde4054704cbdfb663
mungaig <geoffiscoding@gmail.com>
2025-04-10 10:23:29 +0300

    Create 16x16 grid

---
+++ Plan.md
@@ -0,0 +1,127 @@
+###Etch-A-Sketch
+
+##Step 1: Create 16x16 grid
+- Create a grid to occupy the entire container
+ - Use a function that takes an argument.
+ - Use it to create 16 rows and 16 cells in each row.
+
+##Step 1: Psuedocode
+- A function takes a value that the user provides
+- Rows are created based on the value using flexbox
+    - loop through from 1 to the value for this
+- Cells are created inside the rows using flexbox
+    - loop through from 1 to the value for this
+- Call the function with the user input
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-//DOM 
-const divsNum = document.querySelector('#divs-number');
-const divsGen = document.querySelector('#divs-gen');
+// DOM
 const container = document.querySelector('#container');
-const squareRow = document.createElement('div');
-squareRow.id = 'square-row';
+const gridGenBtn = document.querySelector('#grid-btn');
+const userInput = document.querySelector('#user-input');
 
-//Logic
-const containerWidth = container.offsetWidth;
-divsGen.addEventListener('click', () => {
-    container.innerHTML = '';    
-        const squaresNum = divsNum.value; 
-        const squareSide = containerWidth/squaresNum;
-            for(let i = 1; i <= squaresNum; i++){
-                const square = document.createElement('div');
-                square.style.width = squareSide + 'px';
-                square.style.height = squareSide + 'px';
-                square.classList.toggle('square');
-                squareRow.appendChild(square);
-                for(let j = 1; j <= squaresNum; j++){
-                    container.appendChild(squareRow);
-                }
-            divsNum.value = '';
-            divsNum.focus()
-            }                   
- });
+// Logic
+function createGrid(num){
+    //create rows
+    for(let i = 1; i <= num; i++){
+        const row = document.createElement('div');
+        row.classList.toggle('row');
+        container.appendChild(row);
+        //create cells
+        for(let j = 1; j <= num; j++){
+            const cell = document.createElement('div');
+            cell.classList.toggle('cell');
+            row.appendChild(cell);
+        }
+    }
+}
+
+//Call function
+createGrid(16);
