7bf4e3ee065e1d36f4ebe5659e01f141fd2fa4be
mungaig <geoffiscoding@gmail.com>
2025-04-10 07:47:13 +0300

    Remove all logic on JS file

--- script.js
+++ script.js
@@ -2,18 +2,25 @@
 const divsNum = document.querySelector('#divs-number');
 const divsGen = document.querySelector('#divs-gen');
 const container = document.querySelector('#container');
+const squareRow = document.createElement('div');
+squareRow.id = 'square-row';
 
 //Logic
 const containerWidth = container.offsetWidth;
 divsGen.addEventListener('click', () => {
-    container.innerHTML = '';
-    const squaresNum = divsNum.value;
-    for(i = 1; i <= squaresNum; i++){
-        const square = document.createElement('div');
-        square.offsetHeight = containerWidth/squaresNum;
-        square.classList.toggle('square');
-        container.appendChild(square);
-    }
-    divsNum.value = '';
-    divsNum.focus();
-});
+    container.innerHTML = '';    
+        const squaresNum = divsNum.value; 
+        const squareSide = containerWidth/squaresNum;
+            for(let i = 1; i <= squaresNum; i++){
+                const square = document.createElement('div');
+                square.style.width = squareSide + 'px';
+                square.style.height = squareSide + 'px';
+                square.classList.toggle('square');
+                squareRow.appendChild(square);
+                for(let j = 1; j <= squaresNum; j++){
+                    container.appendChild(squareRow);
+                }
+            divsNum.value = '';
+            divsNum.focus()
+            }                   
+ });
